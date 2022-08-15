6ac6b7b92b2533bbfafae7d343c36c99fc2fb484
Praneeth <18praneeth@gmail.com>
2022-08-16 01:54:02 +0530

    updated prog 5

--- Prog5/code.c
+++ Prog5/code.c
@@ -1,98 +1,94 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-char exp1[30],stack[30],arr[30],temp[30];
-int i,k=0,j,l,r,s;
-void push(char exp[])
+int i=0,j=0,k=0,l=0,r=0;
+char inp[30],arr[30],stk[30],temp[30];
+void dispstk()
 {
-	arr[i]=exp1[k];
-	i++;
+    printf("\n");
+    for(k=0;k<strlen(stk);k++)
+        printf("%c",stk[k]);
 }
 void dispinp()
 {
-	printf("\t\t\t");
-	for(k=0;k<strlen(exp1);k++)
-		printf("%c",exp1[k]); printf("$");
-}
-void dispstk()
-{
-	printf("\n");
-	for(k=0;k<strlen(stack);k++)
-		printf("%c",stack[k]);
+    printf("\t\t\t");
+    for(k=0;k<strlen(inp);k++)
+        printf("%c",inp[k]);
+    printf("$");
 }
 void assign()
 {
-	stack[++j]=arr[i];
-	exp1[i]=' ';
-	dispstk();
-	dispinp();
+    stk[++j]=arr[i];
+    inp[i]=' ';
+    dispstk();
+    dispinp();
 }
 int main()
 {
-	printf("\t\t\tSHIFT REDUCE PARSER\n");
-	printf("\nThe Production is: E->E+E/E*E/E-E/i\n");
-	printf("\nEnter the string to be parsed:\n");
-	gets(exp1);
-	printf("\nSTACK\t\t\tINPUT\t\t\tACTION\n");
-	printf("\n$");
-	dispinp();
-	printf("\t\t\tShift");
-	for(k=0;k<strlen(exp1);k++)
-		push(exp1);
-	l=strlen(exp1);
-	stack[0]='$';
-	for(i=0;i<l;i++)
-	{
-		switch(arr[i])
-		{
-			case 'i': 	assign();
-						printf("\t\t\tReduce by E->i");
-						stack[j]='E';
-						dispstk();
-						dispinp();
-						if(arr[i+1]!='\0')
-							printf("\t\t\tShift");
-						break;
-			case '+': 	assign();
-						printf("\t\t\tShift");
-						break;
-			case '*': 	assign();
-						printf("\t\t\tShift");
-						break;
-			case '-': 	assign();
-						printf("\t\t\tShift");
-						break;
-			default: printf("\nError:String not accepted\n");
-			goto label;
-		}
-	}
-	l=strlen(stack);
-	while(l>2)
-	{
-		r=0;
-		for(i=l-1;i>=l-3;i--)
-		{
-			temp[r]=stack[i];r++;
-		}
-		temp[r]=NULL;
-		if((strcmp(temp,"E+E")==0)||(strcmp(temp,"E*E")==0)||(strcmp(temp,"E-E")==0))
-		{
-			for(i=l;i>l-3;i--)
-				stack[i]=' ';
-			stack[l-3]='E';
-			printf("\t\t\tReduce by E->");
-			for(i=0;i<strlen(temp);i++)
-				printf("%c",temp[i]);
-			dispstk();
-			dispinp(); l=l-2;
-		}
-		else
-		{
-			printf("\nError:String not accepted\n"); goto label;
-		}
-	}
-	printf("\t\t\tAccept"); 
-	printf("\n\nString accepted\n");
-	label: exit(0);
-	return 0;
+    printf("\t\t\tGrammer: E -> E+E | E-E | E*E | i\n");
+    printf("Enter the string:\n");
+    gets(inp);
+    for(k=0;k<strlen(inp);k++)
+        arr[k]=inp[k];
+    stk[0]='$';
+    printf("Stack\t\t\tInput\t\t\tAction\n");
+    dispstk();
+    dispinp();
+    printf("\t\t\tShift");
+    for(i=0;i<strlen(arr);i++)
+    {
+        switch(arr[i])
+        {
+            case 'i':
+                assign();
+                printf("\t\t\tReduce by E->i");
+                stk[j]='E';
+                dispstk();
+                dispinp();
+                if(arr[i+1]!='\0')
+                    printf("\t\t\tShift3");
+                break;
+            case '+':
+            case '-':
+            case '*':
+                assign();
+                printf("\t\t\tShift");
+                break;
+            default:
+                printf("\nString not accepeted");
+                return 0;
+        }
+    }
+    l=strlen(stk);
+    while(l>2)
+    {
+        r=0;
+        for(k=l-1;k>=l-3;k--)
+        {
+            temp[r]=stk[k];
+            r++;
+        }
+        if(strcmp(temp,"E+E")==0 || strcmp(temp,"E-E")==0 || strcmp(temp,"E*E")==0)
+        {
+            for(k=l-1;k>=l-3;k--)
+            {
+                stk[k]=' ';
+            }
+            stk[l-3]='E';
+            printf("\t\t\tReduce by E->");
+            for(k=0;k<3;k++)
+                printf("%c",temp[k]);  
+            dispstk();
+            dispinp();  
+        }
+        else
+        {
+            printf("\nString not accepted\n");
+            return 0;
+        }
+        l-=2;
+    }
+    printf("\t\t\tAccept");
+    printf("\nString accepted\n");
+    return 0;
 }
